9838ba4466988a781e5c64dbb78b3cf4c0eae19d
Mikael Brenner <mikaelestavivo@gmail.com>
2020-12-29 17:14:00 -0300

    add heroku configuration

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ const PORT = process.env.PORT || 3000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   console.log(`Server running at port ${PORT}`);
   console.log(
     `The GraphQL Playground can be accessed at http://localhost:${PORT}/graphql`,
